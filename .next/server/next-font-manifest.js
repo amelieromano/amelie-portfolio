f3ef19b82f84e9e72b7a9ed3d9b56b169d9d0d78
Amélie Romano <amelieromano@Amelies-MacBook-Pro.local>
2026-04-24 17:36:35 +0100

    fix: remove .next from git tracking

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/amelieromano/Desktop/amelie-portfolio/app/layout\":[\"static/media/e18f83c737786aa7-s.p.woff2\",\"static/media/64579aaceba9f83a-s.p.woff2\",\"static/media/9dd75fadc5b3df29-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cBgX9Ch4FOzjiH/8LIUzNCmP+SVoH1T89t6fToJr5yY=\"\n}"
